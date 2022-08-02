a1c72f47def407c3e51dedf99f0e9534e00d8ccf
Muhammad Fajar <muhammadfajar191@gmail.com>
2022-08-02 16:34:14 +0700

    show hide password

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -12,9 +12,22 @@
             <div id="error"></div>
         </div>
         <div class="form-group">
-            <input type="password" name="password" class="form-control form-control-user" id="password" placeholder="Masukan Password">
-            <div id="error"></div>
+            <div class="input-group">
+                <input type="password" name="password" class="form-control form-control-user" id="password" placeholder="Masukan Password">
+                <div class="input-group-prepend">
+                    <button type="button" class="input-group-text hidden-password" id="button-password"><i class="fas fa-eye"></i></button>
+                </div>
+                <div id="error"></div>
+            </div>
         </div>
+
+        <!-- <div class="input-group" id="show_hide_password">
+            <input class="form-control" type="password">
+            <div class="input-group-addon">
+                <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
+            </div>
+        </div> -->
+
         <button type="submit" class="btn btn-primary btn-user btn-block">
             Login
         </button>
@@ -22,11 +35,27 @@
 </div>
 <?= $this->endSection() ?>
 
+<?= $this->section('css'); ?>
+<style>
+    #button-password {
+        border-radius: 0 10rem 10rem 0;
+    }
+</style>
+<?= $this->endSection() ?>
+
 <?= $this->section('script'); ?>
 <!-- Custom JS -->
 <script>
     let csrfToken = '<?= csrf_token(); ?>'
     let csrfHash = '<?= csrf_hash(); ?>'
+
+    $('#button-password').on('click', function() {
+        if ($(this).hasClass('hidden-password')) {
+            $(this).removeClass('hidden-password').addClass('show-password').html('<i class="fas fa-eye-slash"></i>').parents('.input-group').find('#password').attr('type', 'text')
+        } else {
+            $(this).removeClass('show-password').addClass('hidden-password').html('<i class="fas fa-eye"></i>').parents('.input-group').find('#password').attr('type', 'password')
+        }
+    })
 
     $('#login-form input').on('keyup', function() {
         $.ajax({
--- app/Views/templates/auth/index.php
+++ app/Views/templates/auth/index.php
@@ -18,6 +18,7 @@
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets'); ?>/css/sb-admin-2.min.css" rel="stylesheet">
 
+    <?= $this->renderSection('css'); ?>
 </head>
 
 <body class="bg-gradient-primary">
